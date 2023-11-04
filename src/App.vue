09d63b4dd9699908c1809c9bc476e54d828e6a53
pedro-dim <pcontato19@gmail.com>
2023-11-04 20:41:18 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+  <template>
+    
+  <div >
+  
+    <Main/>
+   
+  
+  </div>
+  </template>
+
+
+
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
+
+export default{
+
+components:{Main},
+data(){
+
+return{
+
+
+
+}
+
+}
+
+
+}
+
+</script>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template >
+    <div>
+        {{ name }}
+    </div>
+</template>
+
+<script>
+
+    
+    export default {
+      data() {
+        return {
+          name: 'Main Component',
+        };
+      },
+      methods: {
+        doIt() {
+          console.log(`Hello ${this.name}`);
+        },
+      },
+      mounted() {
+        this.doIt();
+      },
+    };
+    </script>
+    
+    <style scoped>
+    </style>
